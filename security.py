106347fb1aff19bdb47a3e6306b8412d52085d71
Drew Laikin <dlaikin3@gmil.com>
2024-01-15 04:59:29 -0500

    fail2ban fix

--- security.py
+++ security.py
@@ -1,28 +1,28 @@
-
 import subprocess
 # Drew Laikin
 # Script to set up security measures for Ubuntu servers
 
 # fail2ban
 subprocess.run(["sudo", "apt", "-y", "install", "fail2ban"])
+subprocess.run(["sudo", "cp", "/etc/fail2ban/fail2ban.conf", "/etc/fail2ban/fail2ban.local"])
 subprocess.run(["sudo", "cp", "/etc/fail2ban/jail.conf", "/etc/fail2ban/jail.local"])
 file = open("/etc/fail2ban/jail.local", "r+")
 lines = file.readlines()
 for each in lines:
-    if "[sshd]" in each:
-        lines.insert(lines.index(each)+1, "enabled = true\nmaxretry = 3\nbantime = 300\n")
+    if "maxretry" in each and "#" not in each:
+        lines.insert(lines.index(each), "maxretry = 2\n")
+        break
 file.seek(0)
 file.writelines(lines)
 file.close()
 subprocess.run(["sudo", "systemctl", "restart", "fail2ban"])
-subprocess.run(["sudo", "systemctl", "enable", "fail2ban"])
 
 # Firewall Setup
 subprocess.run(["sudo", "systemctl","disable", "iptables"])
 subprocess.run(["sudo", "iptables", "-A" , "INPUT", "-j", "DROP"])
 subprocess.run(["sudo", "iptables", "-A" , "INPUT", "-m", "conntrack", "--ctstate", "RELATED,ESTABLISHED", "-j", "ACCEPT"])
 subprocess.run(["sudo", "iptables", "-A" , "INPUT", "-i", "lo", "-j", "ACCEPT"])
-subprocess.run(["sudo", "iptables", "-A" , "INPUT", "-p", "tcp", "--dport", "22", "-j", "ACCEPT"])
+subprocess.run(["sudo", "iptables", "-I" , "INPUT", "-p", "tcp", "--dport", "22", "-j", "ACCEPT"])
 subprocess.run(["sudo", "iptables", "-A" , "INPUT", "-p", "udp", "--dport", "53", "-j", "ACCEPT"])
 subprocess.run(["sudo", "iptables", "-A" , "INPUT", "-p", "tcp", "--dport", "80", "-j", "ACCEPT"])
 subprocess.run(["sudo", "systemctl","enable", "iptables"])
